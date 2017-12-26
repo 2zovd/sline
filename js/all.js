e13170d363d499ebee059465352085754f6a9a09
2zovd <dmy2zov@gmail.com>
2017-12-26 22:27:30 +0200

    fix

---
+++ js/all.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+
+	$('.product-slider').slick({
+		slidesToShow: 1,
+		arrows: true,
+		prevArrow: '<button class="product-arrow prev-arrow"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></button>',
+		nextArrow: '<button class="product-arrow next-arrow"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>',
+		dots: true,
+		autoplay: false
+	});
+
+	(function($){
+		$(window).on("load",function(){
+			$(".scroll-init").mPageScroll2id();
+		});
+	})(jQuery);
+
+	$('.open-popup-link').magnificPopup({
+		type:'inline',
+		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+	});
+
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		if (y > 400) {
+			$('.btn-scroll-top').css('visibility', 'visible');
+		} else {
+			$('.btn-scroll-top').css('visibility', 'hidden');
+		}
+	});
+
+});
+
