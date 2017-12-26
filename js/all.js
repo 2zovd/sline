c44631aeb36018415f344836d1d7dda73beaf83f
Tuzov <dmy2zov@gmail.com>
2017-12-26 19:22:27 +0200

    mobile

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+	
 	$('.product-slider').slick({
 		slidesToShow: 1,
 		arrows: true,
@@ -7,5 +8,17 @@ $( document ).ready(function() {
 		dots: true,
 		autoplay: false
 	});
+	
+	(function($){
+		$(window).on("load",function(){
+			$(".scroll-init").mPageScroll2id();
+		});
+	})(jQuery);
+	
+	$('.open-popup-link').magnificPopup({
+		type:'inline',
+		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+	});
+	
 });
 
