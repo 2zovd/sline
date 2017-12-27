641d0e48f8182fcb337a751d2e0c75cb2cf02a55
Tuzov <dmy2zov@gmail.com>
2017-12-27 11:50:53 +0200

    fixes, zoom

--- js/all.js
+++ js/all.js
@@ -16,8 +16,27 @@ $( document ).ready(function() {
 		midClick: true
 	});
 	
-	$(document).ready(function() {
-		$('.image-link').magnificPopup({type:'image'});
+	// Initialize popup as usual
+	$('.image-link').magnificPopup({
+		type: 'image',
+		mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+		zoom: {
+			enabled: true, // By default it's false, so don't forget to enable it
+
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+
+			// The "opener" function should return the element from which popup will be zoomed in
+			// and to which popup will be scaled down
+			// By defailt it looks for an image tag:
+			opener: function(openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+			}
+		}
+
 	});
 
 	//scroll to top arrow
@@ -28,6 +47,15 @@ $( document ).ready(function() {
 		} else {
 			$('.btn-scroll-top').css('visibility', 'hidden');
 		}
+	});
+	
+	//success message
+	$('.send').click(function() {
+		$('.success-message').addClass('active');
+		setTimeout(function() {
+			$('.success-message').removeClass('active');
+		}, 7000);
+		return false;
 	});
 
 });
